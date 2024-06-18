c239d1862fa4ce6daa887e2535c7b4250ab2ba10
Vaone <gvin8888@gmail.com>
2024-06-18 13:57:21 +0500

    fix: - v3.1 with dotenv

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("config");
 const fileUpload = require("express-fileupload");
 const authRouter = require("./routes/auth.routes");
 const fileRouter = require("./routes/file.routes");
 const app = express();
-const PORT  = process.env.PORT || config.get("serverPort");
-const corsMiddleware = require("./middleware/cors.middleware");
+
 const filePathMiddleware = require("./middleware/filePath.middleware");
 const path = require('path');
+const corsMiddleware = require("./middleware/cors.middleware");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT  = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
+const FILE_PATH = process.env.FILE_PATH;
+const STATIC_PATH = process.env.STATIC_PATH;
 
 app.use(fileUpload({}))
-app.use(corsMiddleware);
-app.use(filePathMiddleware(path.resolve(__dirname, 'files')));
+app.use(filePathMiddleware(path.resolve(__dirname, FILE_PATH)));
+
+app.use(corsMiddleware)
 
 app.use(express.json());
-app.use(express.static('static'));
+app.use(express.static(path.resolve(__dirname, STATIC_PATH)));
 app.use('/api/auth', authRouter);
 app.use('/api/files', fileRouter);
 
@@ -23,7 +30,7 @@ mongoose.set("strictQuery", false);
 
 const start = async () => {
   try {
-    await mongoose.connect(config.get("dbUrl"), {
+    await mongoose.connect(DB_URL, {
       useNewUrlParser:true,
       useUnifiedTopology:true
     });
@@ -35,4 +42,6 @@ const start = async () => {
   }
 };
 
-start();
+start()
+
+module.exports = app
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,43 +2,45 @@ const Router = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("config");
+const dotenv = require("dotenv");
 const {check, validationResult} = require("express-validator");
 const router = new Router();
 const authMiddleware = require('../middleware/auth.middleware');
 const fileService = require('../services/fileService');
 const File = require('../models/File');
 
-router.post('/registration', 
-  [
-    check('email', 'Uncorrected email').isEmail(),
-    check('password', 'Password must be longer than 3 shorter than 12').isLength({min:3, max:12})
-  ],
-  async (req, res) => {
-  try {
-    console.log(req.body)
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(400).json({message: "Uncorrected request", errors})
-    }
-    const {email, password} = req.body
+dotenv.config();
 
-    const candidate = await User.findOne({email})
+router.post('/registration',
+    [
+        check('email', 'Uncorrected email').isEmail(),
+        check('password', 'Password must be longer than 3 shorter than 12').isLength({min:3, max:12})
+    ],
+    async (req, res) => {
+        try {
+            console.log(req.body)
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                return res.status(400).json({message: "Uncorrected request", errors})
+            }
+            const {email, password} = req.body
 
-    if(candidate) {
-      return res.status(400).json({message: `User with email ${email} already exist`})
-    }
-    const hashPassword = await bcrypt.hash(password, 8)
-    const user = new User({email, password: hashPassword})
-    await user.save()
-    await fileService.createDir(new File({user: user.id, name: ''}))
-    return res.json({message: "User was created"})
+            const candidate = await User.findOne({email})
 
-  } catch(e) {
-    console.log(e)
-    res.send({message: "Server error"})
-  }
-})
+            if(candidate) {
+                return res.status(400).json({message: `User with email ${email} already exist`})
+            }
+            const hashPassword = await bcrypt.hash(password, 8)
+            const user = new User({email, password: hashPassword})
+            await user.save()
+            await fileService.createDir(new File({user: user.id, name: ''}))
+            return res.json({message: "User was created"})
+
+        } catch(e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
 
 router.post('/login',
     async (req, res) => {
@@ -52,7 +54,7 @@ router.post('/login',
             if (!isPassValid) {
                 return res.status(400).json({message: "Invalid password"})
             }
-            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: "1h"})
             return res.json({
                 token,
                 user: {
@@ -73,7 +75,7 @@ router.get('/auth', authMiddleware,
     async (req, res) => {
         try {
             const user = await User.findOne({_id: req.user.id})
-            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: "1h"})
             return res.json({
                 token,
                 user: {
