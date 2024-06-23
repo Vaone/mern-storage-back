2f0afe3fefcc07f7bf59441964436be7b27bf3ae
Vaone <gvin8888@gmail.com>
2024-06-23 14:54:59 +0500

    fix: - static path

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ dotenv.config();
 const PORT  = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const FILE_PATH = process.env.FILE_PATH;
+const STATIC_PATH = process.env.STATIC_PATH
 
 app.use(fileUpload({}))
 app.use(filePathMiddleware(path.resolve(__dirname, FILE_PATH)));
@@ -21,10 +22,7 @@ app.use(filePathMiddleware(path.resolve(__dirname, FILE_PATH)));
 app.use(corsMiddleware)
 
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, 'static')));
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/static/', express.static(path.join(__dirname, STATIC_PATH)));
 
 app.use('/api/auth', authRouter);
 app.use('/api/files', fileRouter);
